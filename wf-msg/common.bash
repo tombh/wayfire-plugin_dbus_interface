ad8e04da25d036527c8f95b5c8124db6251ea484
Thomas Buckley-Houston <tom@tombh.co.uk>
2022-05-10 17:25:10 -0300

    wf-msg: dbus-signal wrapper function
    For listening to D-Bus signals

--- wf-msg/common.bash
+++ wf-msg/common.bash
@@ -42,7 +42,46 @@ function dbus-method {
 	fi
 }
 
-function wf-dbus-introspect {
+function dbus-signal {
+	local args
+	declare -A args=(
+		[summary]="Monitor Wayfire's D-Bus interface for specific activity"
+		[details]="$(
+			cat <<-EOM
+				To see all the available signals, use:
+
+				  \`wf-msg dbus-introspect\`
+			EOM
+		)"
+		[0:signal]="Name of signal to monitor"
+		[--timeout]="How long to wait until quitting, default 15 seconds"
+	)
+	__BAPt_parse_arguments args "$@"
+
+	local prefix timeout="${args[timeout]:-15}"
+	prefix=$(
+		_escape_forward_slashes \
+			"/org/wayland/compositor: org.wayland.compositor.${args[signal]} "
+	)
+
+	dbus_cmd=(
+		gdbus
+		monitor
+		--session
+		--object-path /org/wayland/compositor
+		--dest org.wayland.compositor
+	)
+
+	_debug "Monitoring D-Bus for ${args[signal]}"
+	while read -r line; do
+		echo "$line" |
+			grep "${args[signal]}" |
+			sed "s/$prefix//" |
+			_clean
+	done < <(timeout "$timeout" "${dbus_cmd[@]}")
+}
+
+function dbus-introspect {
 	local _
 	declare -A _=(
 		[summary]="Returns XML of all the available Wayfire D-Bus methods and signals"
